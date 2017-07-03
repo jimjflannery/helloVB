c44165cb1be6db4989dbeca3eaec330ad3b749cf
J Flannery <jflannery@cccc.edu>
2017-07-03 12:57:25 -0400

    added another line of code for output

---
+++ Hello.vb
@@ -0,0 +1,17 @@
+
+' J Flannery
+' 5/22/2017
+' Hello World exercise
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Module Hello
+
+    Sub Main()
+        Console.WriteLine("Hello World from Lab 201")
+        Console.WriteLine("Hello World from Lab 201")
+    End Sub
+
+End Module
